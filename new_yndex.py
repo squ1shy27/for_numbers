232c92837a22a6ee5e4dfe44064ad64e027ab8ac
squ1shy27 <rybalkin.kirya@mail.ru>
2024-07-06 09:18:44 +0300

    optimize key logic

--- new_yndex.py
+++ new_yndex.py
@@ -5,12 +5,12 @@ number = randint(1, 100)
 print('ygadaite chislo ot 1 do 100')
 
 while True:
-    guess = int(input('vvedite chislo'))
+    guess = int(input('vvedite chislo '))
     if guess < number:
         print('vashe chislo menshe')
-    if guess > number:
+    elif guess > number:
         print('vashe chislo bolshe')
-    if guess == number:
+    elif guess == number:
         break
 
 
